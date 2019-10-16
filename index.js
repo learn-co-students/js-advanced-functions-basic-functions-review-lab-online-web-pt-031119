f8e07e81ccf156c5c9a07afa40ff42e0937a17c4
“Mark <mkanko66@gmail.com>
2019-10-16 16:02:37 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const saturdayFun = (fun="roller-skate") => {
+    return `This Saturday, I want to ${fun}!`
+}
+
+ const mondayWork = (work='go to the office') => {
+    return `This Monday, I will ${work}.`
+}
+
+const wrapAdjective = (flair='*') => {
+    return function(param='special') {
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(x, y) {return x + y},
+    subtract: function(x, y) {return x - y},
+    multiply: function(x,y) {return x * y},
+    divide: function(x, y) {return x / y}  
+} 
+
+const actionApplyer = (num, array) => {    
+    for (const element of array) {
+        num = element(num)
+    }
+    return num  
+}
+
+
+
+
+
